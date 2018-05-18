6bc21d5a6ee7e08b1780844db060405c7e4c68e3
Mark Jung <gujung2022@u.northwestern.edu>
2018-05-17 23:10:36 -0500

    done until pawn-loc

--- TsuroTheSecond/Classes/Parser.cs
+++ TsuroTheSecond/Classes/Parser.cs
@@ -25,15 +25,35 @@ namespace TsuroTheSecond
 
             XmlNodeList list_of_colorXML = input.SelectSingleNode("/initialize/list").ChildNodes;
             XmlNode first = input.SelectSingleNode("/initialize").FirstChild;
-            string own_color = input.SelectSingleNode("/initialize").FirstChild.InnerText.Replace(" ", "");
+            string own_color = this.ColorXML(input.SelectSingleNode("/initialize").FirstChild);
 
             List<string> list_of_color = new List<string>();
 
             foreach(XmlNode each in list_of_colorXML){
-                list_of_color.Add(each.InnerText.Replace(" ", ""));
+                list_of_color.Add(this.ColorXML(each));
             }
 
             return (own_color, list_of_color);
+        }
+
+        public string ColorXML(XmlNode colorXML){
+            return colorXML.InnerText.Replace(" ", "");
+        }
+
+        public int NXML(XmlNode n){
+            return int.Parse(n.InnerText);
+        }
+
+        public string HVXML(XmlNode hv){
+            return hv.Name;
+        }
+
+        public (string, int, int) PawnLocXML(XmlNode pawn_loc){
+            XmlNodeList pawn_child = pawn_loc.ChildNodes;
+            string horv = this.HVXML(pawn_child.Item(0));
+            int x = this.NXML(pawn_child.Item(1));
+            int y = this.NXML(pawn_child.Item(2));
+            return (horv, x, y);
         }
         
 
--- TsuroTheSecondTests/ParserTest.cs
+++ TsuroTheSecondTests/ParserTest.cs
@@ -29,7 +29,8 @@ namespace TsuroTheSecondTests
         //    //}
         //}
         [TestMethod]
-        public void TestParserGetCommand(){
+        public void TestParserGetCommand()
+        {
             XElement get_name = XElement.Parse("<get-name> </get-name>");
             XmlDocument xmlDocument = new XmlDocument();
             xmlDocument.LoadXml(get_name.ToString());
@@ -42,14 +43,82 @@ namespace TsuroTheSecondTests
         [TestMethod]
         public void TestParserInitialize()
         {
-            XElement get_name = XElement.Parse("<initialize> <color> blue </color> <list> <color> red </color> <color> blue </color> <color> green </color> </list> </initialize>");
+            XElement initialize = XElement.Parse("<initialize> <color> blue </color> <list> <color> red </color> <color> blue </color> <color> green </color> </list> </initialize>");
             XmlDocument xmlDocument = new XmlDocument();
-            xmlDocument.LoadXml(get_name.ToString());
+            xmlDocument.LoadXml(initialize.ToString());
             Parser parser = new Parser();
             (string own_color, List<string> other_colors) = parser.InitializeXML(xmlDocument);
             Assert.AreEqual("blue", own_color);
-            CollectionAssert.AreEqual(new List<string>{"red", "blue", "green"}, other_colors);
+            CollectionAssert.AreEqual(new List<string> { "red", "blue", "green" }, other_colors);
         }
 
+        [TestMethod]
+        public void TestParserPlacePawn()
+        {
+            // place-pawn contains a board so it should output a board object.
+
+        }
+
+        [TestMethod]
+        public void TestParserN()
+        {
+            string xmlContent = "<n>5</n>";
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlContent);
+            XmlNode newNode = doc.DocumentElement;
+            Parser parser = new Parser();
+            int nat = parser.NXML(newNode);
+            Assert.AreEqual(5, nat);
+        }
+
+        [TestMethod]
+        public void TestParserHV_H()
+        {
+            string xmlContent = "<h></h>";
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlContent);
+            XmlNode newNode = doc.DocumentElement;
+            Parser parser = new Parser();
+            string horv = parser.HVXML(newNode);
+            Assert.AreEqual("h", horv);
+        }
+
+        [TestMethod]
+        public void TestParserHV_V()
+        {
+            string xmlContent = "<v></v>";
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlContent);
+            XmlNode newNode = doc.DocumentElement;
+            Parser parser = new Parser();
+            string horv = parser.HVXML(newNode);
+            Assert.AreEqual("v", horv);
+        }
+
+        [TestMethod]
+        public void TestParserPawnLoc()
+        {
+            string xmlContent = "<pawn-loc><h></h><n>3</n><n>4</n></pawn-loc>";
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlContent);
+            XmlNode newNode = doc.DocumentElement;
+            Parser parser = new Parser();
+            (string horv, int x, int y) = parser.PawnLocXML(newNode);
+            Assert.AreEqual("h", horv);
+            Assert.AreEqual(3, x);
+            Assert.AreEqual(4, y);
+        }
+
+        [TestMethod]
+        public void TestParserPawns()
+        {
+            string xmlContent = "<map><ent><color>blue</color><pawn-loc><h></h><n>3</n><n>4</n></pawn-loc></ent> <ent><color>red</color><pawn-loc><v></v><n>4</n><n>3</n></pawn-loc></ent></map>";
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlContent);
+            XmlNode newNode = doc.DocumentElement;
+            Parser parser = new Parser();
+            //IN PROGRESS
+            //
+        }
     }
 }
