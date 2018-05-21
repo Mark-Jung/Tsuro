87cf645519c29866b8588570dd18e78de0eda692
Mark Jung <gujung2022@u.northwestern.edu>
2018-05-21 15:22:01 -0500

    untested maker of xml

--- TsuroTheSecond/Classes/Maker.cs
+++ TsuroTheSecond/Classes/Maker.cs
@@ -8,13 +8,53 @@ namespace TsuroTheSecond
         {
         }
 
-        public string PlayerName(string name){
-            XElement playername = new XElement("player-name", name);
-            return playername.ToString();
+        public XElement PlayerNameXML(string playername){
+            XElement playernamexml = new XElement("player-name", playername);
+            return playernamexml;
         }
 
+        public XElement NXML(int n) {
+            XElement nxml = new XElement("n", n);
+            return nxml;
+        }
 
+        public XElement ConnectXML(XElement n1, XElement n2) {
+            XElement connectxml = new XElement("connect",
+                                           n1,
+                                           n2
+                                           );
+            return connectxml;
+        }
 
+        public XElement TileXML(XElement connect1, XElement connect2, XElement connect3, XElement connect4) {
+            XElement tilexml = new XElement("tile",
+                                            connect1,
+                                            connect2,
+                                            connect3,
+                                            connect3,
+                                            connect4
+                                           );
+            return tilexml;
+        }
+
+        public XElement HVXML(string horv){
+            if(horv == "h"){
+                XElement hxml = new XElement("h");
+                return hxml;
+            } else {
+                XElement vxml = new XElement("v");
+                return vxml;
+            }
+        }
+
+        public XElement PawnLocXML(XElement horv, XElement x, XElement y) {
+            XElement pawnlocxml = new XElement("pawn-loc",
+                                               horv,
+                                               x,
+                                               y
+                                              );
+            return pawnlocxml; 
+        }
 
     }
 }
