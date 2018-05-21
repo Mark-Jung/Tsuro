808f3c4e35963ebc7016d4dec71cdaf2d6973514
Mark Jung <gujung2022@u.northwestern.edu>
2018-05-21 14:53:55 -0500

    started on formulatding xml and fixed paly-turn specifications

---
+++ TsuroTheSecond/Classes/Maker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Xml.Linq;
+namespace TsuroTheSecond
+{
+    public class Maker
+    {
+        public Maker()
+        {
+        }
+
+        public string PlayerName(string name){
+            XElement playername = new XElement("player-name", name);
+            return playername.ToString();
+        }
+
+
+
+
+    }
+}
